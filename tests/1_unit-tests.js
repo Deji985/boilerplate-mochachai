bfbe7419b5ea70f96024c649953bdfc6bcfe3830
Deji985 <akerejola.ayodeji@acity.edu.gh>
2026-03-29 18:44:19 +0000

    new

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -10,9 +10,9 @@ suite('Unit Tests', function () {
     });
     // #2
     test('#isDefined, #isUndefined', function () {
-      assert.fail(null, 'null is not undefined');
-      assert.fail(undefined, 'undefined IS undefined');
-      assert.fail('hello', 'A string is not undefined');
+      assert.isDefined(null);
+      assert.isUndefined(undefined);
+      assert.isDefined('hello');
     });
     // #3
     test('#isOk, #isNotOk', function () {
